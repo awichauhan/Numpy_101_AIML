910d2ba241ee7e3e4ea7937ec68b1b108d8f5673
awi_chauhan <monasingh8980@gmail.com>
2026-07-12 13:10:47 +0530

    zero dimensional array

---
+++ ZeroDimensionalArray.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+value = np.array(10)
+print(value)
+print("Dimensions", value.ndim)
+print("Shape", value.shape)
+
+
+# This contains only one scalar value.
+# It has no axis, so it is called a 0D array.
+
