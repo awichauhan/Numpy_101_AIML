26439e64e6ea4261322545ff42abc909873d6c7c
awi_chauhan <monasingh8980@gmail.com>
2026-07-12 13:15:01 +0530

    one dimension array and shape is stored in form of tuple

---
+++ OneDimensionalArray.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+oneDimArray = np.array([10,20,30])
+print("One Dimensional Array", oneDimArray)
+print("Dimensions", oneDimArray.ndim)
+print("Shape", oneDimArray.shape)
+
+
+# Shape is (3,0): The comma in (3,) indicates that this is a one-dimensional shape "tuple"
+
