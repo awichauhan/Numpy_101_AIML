bd47562733d91da5cda1b05e30a730d8bd0f10ca
awi_chauhan <monasingh8980@gmail.com>
2026-07-12 13:20:10 +0530

    2D arrays and their shape is (rows,columns)

---
+++ TwoDimensionalArray.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+TwoDMatrix = np.array([
+    [10,20,30],
+    [40,50,60]
+])
+
+print("2D Array is \n", TwoDMatrix)
+print("Shape", TwoDMatrix.shape)
+print("Dimension", TwoDMatrix.ndim)
+
+
+# shape = (rows, columns), here it is (2,3)
+
