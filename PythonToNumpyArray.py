f57ce8e463434fbdd6ebefc262dc15c01d41a577
awi_chauhan <monasingh8980@gmail.com>
2026-07-12 13:06:25 +0530

    Python array to Numpy array

---
+++ PythonToNumpyArray.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+pythonArray = [10,20,30,40]
+print(type(pythonArray))
+numpyArray = np.array(pythonArray)
+print(numpyArray)
+print(type(numpyArray))
+
+#output:
+# <class 'list'>
+# [10 20 30 40]
+# <class 'numpy.ndarray'>
+
