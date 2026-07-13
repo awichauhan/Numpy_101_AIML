30fe7b88b1275bbf8cc49f7a6c1738763424aa1b
awi_chauhan <monasingh8980@gmail.com>
2026-07-14 00:12:06 +0530

    specifying data type while initialing numpy array

--- ZeroDimensionalArray.py
+++ ZeroDimensionalArray.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-value = np.array(10)
+value = np.array(10 , dtype="int16")  # specifying data type while initializing; int16 as we know that the values will be smaller
 print(value)
 print("Dimensions", value.ndim)
 print("Shape", value.shape)
+print("datatype: ", value.dtype)
 
 
 # This contains only one scalar value.
